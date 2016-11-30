c17235c74c4e89c97c23b00d119465942e14d487
frodzet <simon.birkedal@gmail.com>
2016-11-30 10:48:18 +0100

    Added SongReadProperty

--- src/mytunes/gui/controller/AddSongViewController.java
+++ src/mytunes/gui/controller/AddSongViewController.java
@@ -21,16 +21,21 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 import mytunes.be.Song;
+import mytunes.dal.ReadSongProperty;
 import mytunes.gui.model.SongModel;
-import org.farng.mp3.MP3File;
-import org.farng.mp3.TagException;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.TagException;
 
 /**
  * FXML Controller class
  *
  * @author James
  */
-public class AddSongViewController implements Initializable {
+public class AddSongViewController implements Initializable
+{
+    private ReadSongProperty rsp;
 
     @FXML
     private TextField txtTitle;
@@ -39,8 +44,9 @@ public class AddSongViewController implements Initializable {
     @FXML
     private TextField txtGenre;
     @FXML
+    private TextField txtDuration;
+    @FXML
     private TextField txtPath;
-
     @FXML
     private Button closeButton;
 
@@ -59,69 +65,68 @@ public class AddSongViewController implements Initializable {
     {
         songModel = SongModel.getInstance();
 
+        txtPath.textProperty().addListener((observable, oldValue, newValue)
+                -> 
+                {
+                    if (newValue.isEmpty())
+                    {
+                        txtTitle.setDisable(true);
+                        txtArtist.setDisable(true);
+                        txtGenre.setDisable(true);
+                    }
+                    else
+                    {
+                        txtTitle.setDisable(false);
+                        txtArtist.setDisable(false);
+                        txtGenre.setDisable(false);
+                    }
+        });
     }
 
     @FXML
     private void closeWindow()
     {
         Stage stage = (Stage) closeButton.getScene().getWindow();
+        // do what you have to do
         stage.close();
     }
 
     @FXML
-    public void addSong() throws IOException, TagException
+    public void addSong()
     {
         String title = txtTitle.getText();
         String artist = txtArtist.getText();
         String genre = txtGenre.getText();
+        String duration = txtDuration.getText();
         String path = txtPath.getText();
-//        MP3File file = new MP3File(path);
-//        long audioFileLength = file.getID3v2Tag().getSize();
-//        int frameSize = file.getID3v2Tag().getFrameCount();
-//        float durationInSeconds = (audioFileLength / frameSize);
-        song = new Song(title, artist, genre, 4.20, 0, path);
+
+        song = new Song(title, artist, genre, duration, 0, path);
         songModel.addSong(song);
         closeWindow();
 
     }
 
     @FXML
-    private void browseForFile(ActionEvent event) throws IOException, TagException
+    private void browseForFile(ActionEvent event)
     {
-        FileChooser fileChooser = new FileChooser();
-        Window win = root.getScene().getWindow();
-        File file = fileChooser.showOpenDialog(win);
-        txtPath.setText(file.getPath());
-        String title;
-        String artist;
-        String genre;
+        try
+        {
+            FileChooser fileChooser = new FileChooser();
+            Window win = root.getScene().getWindow();
+            File file = fileChooser.showOpenDialog(win);
+            txtPath.setText(file.getPath());
 
-        MP3File mp3file = new MP3File(file);
-        System.out.println(mp3file.toString());
-        title = mp3file.getID3v2Tag().getSongTitle();
-        artist = mp3file.getID3v2Tag().getLeadArtist();
-        genre = mp3file.getID3v2Tag().getSongGenre();
+            rsp = new ReadSongProperty(file.getPath());
+            txtTitle.setText(rsp.getTitle());
+            txtArtist.setText(rsp.getArtist());
+            txtGenre.setText(rsp.getGenre());
+            txtDuration.setText(rsp.getDuration());
 
-        txtTitle.setText(title);
-        txtArtist.setText(artist);
-        txtGenre.setText(genre);
-        
-        
-//        long audioFileLength = mp3file.getID3v2Tag().getSize();
-//        int frameSize = mp3file.getID3v2Tag().getFrameCount();
-//        float durationInSeconds = (audioFileLength / frameSize);
-//        
-//        int length = 254;
-//        int result = 15;
-//        int some = (int) (audioFileLength-(frameSize*(frameSize+2)));
-//        
-//        System.out.println(audioFileLength);
-//        System.out.println(frameSize);
-//        System.out.println(durationInSeconds);
-//        
-//        System.out.println(some);
-        
-      
+        }
+        catch (CannotReadException | IOException | TagException | ReadOnlyFileException | InvalidAudioFrameException ex)
+        {
+            // TODO: Handling the different exceptions.
+        }
     }
 
 }
